aa06fe59bee3beb58fbeeaf3fb3f5dcdf3924675
Balogun Tobi <mastertbal@gmail.com>
2021-11-25 01:44:31 +0100

    Wrote the logic of conrolling drop-down behaviour of the menu icon

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 var menuIcon = document.getElementById("menu");
+menuIcon.onclick = 
+function()
+{
+    var myUl = document.getElementById("list-container");
+    if(myUl.style.display == "none") 
+    {
+        myUl.style.backgroundColor = "white";
+        myUl.style.padding = "10px";
+        myUl.style.display = "block";
+        myUl.style.zIndex = "1";
+    }
+    else
+    {
+        myUl.style.display = "none";
+    }
+}
